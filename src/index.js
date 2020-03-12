6283b79b28440185fd332760e000fb82b425ddf3
Andrey S <nukemoore@gmail.com>
2020-03-12 14:28:31 +0300

    I know useState

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+    return (
+        <>
+        <HookSwitcher />
+        </>
+    );
+}
+
+const HookSwitcher = () => {
+    const [color, setColor] = useState('white');
+    const [fontSize, setFontSize] = useState(14);
+
+    function setSize(e) {
+        let value = Number(e.target.value);
+        setFontSize(value);
+    }
+
+    return (
+        <div style={{padding: '10px', 
+                    backgroundColor: color,
+                    fontSize: `${fontSize}px`}}>
+            Hello world!<br></br>
+            <button 
+            onClick={() => setColor('red')}>Red</button>
+            <button 
+            onClick={() => setColor('green')}>Green</button>
+            <button 
+            onClick={() => setFontSize((fontSize) => fontSize + 2)}>Bigger</button>
+            <button 
+                onClick={() => setFontSize((fontSize) => fontSize - 2)}>Smaller</button>
+            <input onKeyUp={(e) => setSize(e)}></input>
+        </div>
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
